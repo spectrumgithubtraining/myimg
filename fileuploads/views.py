4c05fdb13a607b29360a9d5b80180fbeb4c2bd74
spectrumgithubtraining <githubtraining@spectrum.in>
2023-12-05 14:20:45 +0530

    first commit

---
+++ fileuploads/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import UploadedFile
+from .forms import UploadFileForm
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('upload_file')
+    else:
+        form = UploadFileForm()
+    files = UploadedFile.objects.all()
+    return render(request, 'upload_file.html', {'form': form, 'files': files})
+
+def download_file(request, file_id):
+    uploaded_file = UploadedFile.objects.get(pk=file_id)
+    response = HttpResponse(uploaded_file.file, content_type='application/force-download')
+    response['Content-Disposition'] = f'attachment; filename="{uploaded_file.file.name}"'
+    return response
